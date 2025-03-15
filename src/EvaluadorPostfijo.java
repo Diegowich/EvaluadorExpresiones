d24759b775416a833a05f25c2df7fdc73f24e249
Diegowich <deagle.sanctum@gmail.com>
2025-03-14 21:43:53 -0500

    Codificado evaluador de resultado en postfijo

--- src/EvaluadorPostfijo.java
+++ src/EvaluadorPostfijo.java
@@ -2,5 +2,35 @@ import java.util.Stack;
 
 public class EvaluadorPostfijo {
 
+   public static int solucionarTermino(String expresion){
+
+        Stack<Integer> pila = new Stack<>();
+
+        int resultado = 0;
+        for(char c:expresion.toCharArray()){
+            if (Character.isDigit(c)){
+                pila.push(c - '0');
+            }
+            else {
+                if(pila.size()>= 2){
+                    int operando2 = pila.pop();
+                    int operando1 = pila.pop();
+
+                    switch(c){
+                        case '+' ->  resultado = operando1 + operando2;
+                        case '-' ->  resultado = operando1 - operando2;
+                        case '*' -> resultado = operando1 * operando2;
+                        case '/' -> resultado = operando1 / operando2;
+                        default -> throw new IllegalArgumentException("Operador inválido: " + c);
+                    }
+                }   pila.push(resultado);
+
+            }
+        }
+        if(pila.size() != 1){
+            throw new IllegalArgumentException("Expresion prefija inválida.");
+        }
+        return pila.pop();
+   }
 }
 
